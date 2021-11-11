5cf46bf31f278cf024897710979e1d9d7cfb9042
skadndtlr97 <skadndtlr97@gmail.com>
2021-11-11 16:27:25 +0900

    home

--- src/main/java/com/hago/getcha/HomeController.java
+++ src/main/java/com/hago/getcha/HomeController.java
@@ -33,7 +33,7 @@ public class HomeController {
 		
 		model.addAttribute("serverTime", formattedDate );
 
-		return "restManagement/menuRegisterForm";
+		return "restManagement/menuRegisterForm12";
 
 	}
 	
