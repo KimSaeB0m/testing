f65526fb7b8e4b651b3c200d1a59a83a943e07dd
skadndtlr97 <skadndtlr97@gmail.com>
2021-11-11 15:20:31 +0900

    ㅁㄴㅇ

--- src/main/java/com/hago/getcha/HomeController.java
+++ src/main/java/com/hago/getcha/HomeController.java
@@ -32,7 +32,7 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		return "home/dfgdg";
+		return "home/dfgd";
 	}
 	
 	@RequestMapping(value="/member")
