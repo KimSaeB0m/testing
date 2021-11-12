376f510f3c8e4a8965ca6ffa758a03a3c5e8b3f0
DESKTOP-7B62DVS\남웅식 <skadndtlr97@gmai.com>
2021-11-12 21:39:41 +0900

    asd

--- src/main/java/com/hago/getcha/HomeController.java
+++ src/main/java/com/hago/getcha/HomeController.java
@@ -33,7 +33,7 @@ public class HomeController {
 		
 		model.addAttribute("serverTime", formattedDate );
 
-		return "restManagement/menuRegisterForm1";
+		return "restManagement/menuRegisterForm";
 
 	}
 	
